028c34373712792e6fd923aac1e300680dfb5377
pavan chitumalla <pavan@pinterest.com>
2017-09-05 14:21:32 -0700

    fix waiter

--- testserver/src/main/java/SimpleServer.java
+++ testserver/src/main/java/SimpleServer.java
@@ -1,29 +1,32 @@
-import com.twitter.finagle.stats.InMemoryStatsReceiver;
-import com.twitter.finagle.stats.StatsReceiver;
 import com.twitter.server.AbstractTwitterServer;
+import com.twitter.util.Await;
+import com.twitter.util.TimeoutException;
 
 public class SimpleServer extends AbstractTwitterServer {
+
+  @Override
+  public void main() {
+    try {
+      Await.ready(this.adminHttpServer());
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (TimeoutException e) {
+      e.printStackTrace();
+    }
+  }
 
   public static class Main {
 
     public static void main(String[] args) throws InterruptedException {
-      Thread waiterThread = new Thread(new Waiter());
-      waiterThread.start();
-      StatsReceiver isr = new InMemoryStatsReceiver();
-      SimpleServer ss = new SimpleServer();
-      ss.main(args);
-      waiterThread.join();
+
+      //StatsReceiver isr = new InMemoryStatsReceiver();
+      new SimpleServer().main(args);
     }
   }
 
-  static class Waiter implements Runnable {
-
-    public void run() {
-      try {
-        Thread.sleep(100000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
+  @Override
+  public void onInit() {
+    log().info("Java Server initialization...");
   }
+
 }
